ca6c3733421d7141a49562c95e4fab29b4414f62
swarnimsurvase14 <swarnim450sng@gmail.com>
2026-01-07 14:25:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -250,11 +250,3 @@ def retrospective_handler():
 # --- STARTUP COMMAND FOR RENDER (Gunicorn) ---
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
-
-
-
-
-
-
-
